f9ebe7352220e8aa834de6caea40ed2047ee7a1e
DIMAS MAHESA <96423520+Dimjol@users.noreply.github.com>
2024-07-15 00:14:03 +0700

    Data Driven Automation Katalon

---
+++ Object Repository/Test Login/Page_Dummy Login Page/button_Sign Out.rs
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Sign Out</name>
+   <tag></tag>
+   <elementGuidId>1c522c9e-0fa3-4172-af9f-f7e48e411a2b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>button.form__button</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@id='link-back']/button</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=button[name=&quot;Sign Out&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>2f6f8e7e-68bf-4db1-8d58-810c771ba0eb</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>form__button</value>
+      <webElementGuid>fdca786f-5ed4-4698-a2f1-b57873e8d56c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                        Sign Out
+                    </value>
+      <webElementGuid>c609084f-0027-4aec-a4cc-a6aad6e1a3bd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;link-back&quot;)/button[@class=&quot;form__button&quot;]</value>
+      <webElementGuid>7cec322e-07cb-4b93-bc48-ace623bc234b</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//a[@id='link-back']/button</value>
+      <webElementGuid>cc0e7dd8-d7fc-48a6-bc01-866955fc60bc</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Login Successful'])[1]/following::button[1]</value>
+      <webElementGuid>40357e03-3e24-4f3e-bddc-bf3894ff1e3d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Back to Website'])[1]/preceding::button[1]</value>
+      <webElementGuid>64bf3db8-d9c1-46b1-aa4e-ed8d41725628</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Sign Out']/parent::*</value>
+      <webElementGuid>4e0cfd51-5327-4a39-a589-b2a9fa3f4eda</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//button</value>
+      <webElementGuid>921fe24e-e9cb-4ce3-8713-2845816cf3e9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[(text() = '
+                        Sign Out
+                    ' or . = '
+                        Sign Out
+                    ')]</value>
+      <webElementGuid>d4cb2739-9fcf-494c-a44d-87d9b52f583e</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/Data Driven Test.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Data Driven Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>4b34cdaa-004b-4feb-b177-a7b4fddb9851</testSuiteGuid>
+   <testCaseLink>
+      <guid>0924f196-292e-48e1-9d29-63f25f8e90a8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>785aafa8-77cf-4443-bfbb-f2c50ca76510</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Login Data/Login Data</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>785aafa8-77cf-4443-bfbb-f2c50ca76510</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>User Name</value>
+         <variableId>ee94f155-c667-4dc7-9658-c172b547132d</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>785aafa8-77cf-4443-bfbb-f2c50ca76510</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>f9c64efe-7a89-4bc5-b2a9-9fea8ca578e5</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
